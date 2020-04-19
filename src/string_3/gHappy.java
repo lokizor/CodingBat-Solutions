cd07e75bf1785ed8deae5c5518d2fe7c145500ce
zoan <zoan@zoanpc>
2020-04-19 14:23:07 +0200

    solution gHappy

---
+++ src/string_3/gHappy.java
@@ -0,0 +1,52 @@
+package string_3;
+/*
+ * We'll say that a lowercase 'g' in a string is "happy" if there is another 'g' immediately to its left or right. 
+ * Return true if all the g's in the given string are happy.
+
+	gHappy("xxggxx") → true
+	gHappy("xxgxx") → false
+	gHappy("xxggyygxx") → false
+ */
+public class gHappy {
+	
+	public static void main(String[] args) {
+		
+		System.out.println(gHappy("xxggxx"));
+		System.out.println(gHappy("xxgxx"));
+		System.out.println(gHappy("xxggyygxx"));
+		
+		
+	}
+	
+	
+	public static boolean gHappy(String str) {
+		
+		char[] arr = str.toCharArray();
+		
+		for (int i = 1; i < arr.length; i++) {
+			
+			if (arr[i] == 'g') {
+				
+				//xxgg
+				if (i+1 == arr.length) {
+					if (!(arr[i-1] =='g')) {
+						return false;
+					}
+				} else {
+									}
+				
+				
+				if (arr[i-1] != 'g' && arr[i+1] != 'g') {
+					return false; 
+				}
+				
+			}
+			
+			
+			
+		}
+		
+		return true;
+	}
+
+}
